e44755ecaadb4794709c6546a8618e44ca1f902b
Douglas Costa <dodop3000@gmail.com>
2014-07-24 16:13:12 -0300

    spec from github

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 feature 'Creating Projects' do
-	scenario 'can create a project' do
-		visit '/'
+  scenario 'can create a project' do
+    visit '/'
 
-		click_link 'New Project'
+    click_link 'New Project'
 
-		fill_in 'Name', with: 'TextMate 2'
-		fill_in 'Description', with: 'A text-editor'
-		click_button 'Create Project'
+    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Description', with: 'A text-editor'
+    click_button 'Create Project'
 
-		expect(page).to have_content('Project has been created.')
-		
-	end
+    expect(page).to have_content('Project has been created.')
+    
+  end
 end
