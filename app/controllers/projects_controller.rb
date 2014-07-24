f80da9868656f81d1ecc99a4d2f8530a3835f4ed
Douglas Costa <dodop3000@gmail.com>
2014-07-24 16:38:56 -0300

    akita

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,14 @@
 class ProjectsController < ApplicationController
-	def index	
-	end
-
-	def show
-		@project = Project.find(params[:id])
+  def index	
   end
 
-	def new
-		@project = Project.new
-	end
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
 
 	def create
 		@project = Project.new(project_params)
