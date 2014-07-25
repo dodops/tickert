852b5a30db4645e500448d62e3152db915616e55
Douglas Costa <dodop3000@gmail.com>
2014-07-25 19:06:15 -0300

    testing unit validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+    it 'persists a user with valid attributes' do
+      project = Project.create(name: "TextMate", description: "Smeels like")
+
+      expect(project).to be_persisted
+    end
+
+    it 'does not persist if attributes are not valid' do
+      project = Project.create
+
+      expect(project).not_to be_persisted
+      expect(project.errors[:name]).to include("can't be blank")
+    end
+  end
 end
