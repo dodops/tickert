6ca5d04ce95710c5e93420a3317d38fa0429fb54
Douglas Costa <dodop3000@gmail.com>
2014-07-26 10:09:00 -0300

    new feature

---
+++ spec/features/creating_tickets_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature "CreatingTickets", :type => :feature do
+  before do
+    FactoryGirl.create(:project, name: "Internet Explorer", description: 'awful')
+
+
+    visit '/'
+    click_link "Internet Explorer"
+    click_link "New Ticket"
+  end
+
+  scenario 'creating a ticket' do
+    context 'with valid attributes' do
+      fill_in "Title", with: "Non-stards compliance"
+      fill_in "Description", with: "My pages are ugly"
+      click_button "Create Ticket"
+
+      expect(page).to have_content("Ticket has been created.")
+    end
+
+    context 'with invalid attributes, must fail' do
+      click_button 'Create Ticket'
+
+      expect(page).to have_content("Ticket has not been created.")
+      expect(page).to have_content("Title can't be blank")
+    end
+  end
+end
