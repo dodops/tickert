2b5a8819a60c78f98eb067a169247c09c32d9b93
Douglas Costa <dodop3000@gmail.com>
2014-07-26 10:57:40 -0300

    Implement creating tickets for a project

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,38 @@
+class TicketsController < ApplicationController
+  before_action :find_project
+  before_action :find_ticket, only: [:show, :edit, :update, :destroy]
+
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def show
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:notice] = "Ticket has not been created."
+      render 'new'
+    end
+  end
+
+
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
+  end
+
+  def find_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -4,26 +4,25 @@ feature "CreatingTickets", :type => :feature do
   before do
     FactoryGirl.create(:project, name: "Internet Explorer", description: 'awful')
 
-
     visit '/'
     click_link "Internet Explorer"
     click_link "New Ticket"
   end
 
   scenario 'creating a ticket' do
-    context 'with valid attributes' do
-      fill_in "Title", with: "Non-stards compliance"
-      fill_in "Description", with: "My pages are ugly"
-      click_button "Create Ticket"
 
-      expect(page).to have_content("Ticket has been created.")
-    end
+    fill_in "Title", with: "Non-stards compliance"
+    fill_in "Description", with: "My pages are ugly"
+    click_button "Create Ticket"
 
-    context 'with invalid attributes, must fail' do
-      click_button 'Create Ticket'
+    expect(page).to have_content("Ticket has been created.")
+  end
 
-      expect(page).to have_content("Ticket has not been created.")
-      expect(page).to have_content("Title can't be blank")
-    end
+  scenario 'create a ticket with invalid attributes, must fail' do
+
+    click_button 'Create Ticket'
+
+    expect(page).to have_content("Ticket has not been created.")
+    expect(page).to have_content("Title can't be blank")
   end
 end
