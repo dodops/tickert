1e472310ef123e7a790be771eb7da7591de0af54
Douglas Costa <dodop3000@gmail.com>
2014-07-28 13:20:48 -0300

    refactoring controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, except: [:new, :create]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def create
@@ -17,11 +18,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Profile has been updated."
     else
@@ -31,6 +30,10 @@ class UsersController < ApplicationController
 
 
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation )
