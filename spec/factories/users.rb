5853a671f82f703ef49f1beeee96dc59700448bd
Douglas Costa <dodop3000@gmail.com>
2014-07-28 15:27:19 -0300

    implement signing in.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,6 @@ FactoryGirl.define do
     name "MyString"
     email "MyString"
     password_digest "MyString"
+    password_confirmation "MyString"
   end
 end
--- spec/features/signins_spec.rb
+++ spec/features/signins_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 feature "Signins", :type => :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "Sign in." do 
+    user = FactoryGirl.create(:user, password: "poder", password_confirmation: "poder")
+
+    visit '/'
+    click_link "Sign in"
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+
+    expect(page).to have_content("Signed in successfully.")
+  end
 end
