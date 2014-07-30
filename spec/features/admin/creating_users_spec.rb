dd69d84cc5393e13e57012291fbb5e1622e616ff
Douglas Costa <dodop3000@gmail.com>
2014-07-30 14:27:19 -0300

    Add admin users through the admin backend

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -17,4 +17,12 @@ feature "CreatingUsers", :type => :feature do
     click_button "Create User"
     expect(page).to have_content("User has been created.")
   end
+
+  scenario "Creating an admin user" do
+    fill_in "Email", with: "admin@exmaple.com"
+    fill_in "Password", with: "password"
+    check "Is an admin?"
+    click_button "Create User"
+    expect(page).to have_content("admin@exmaple.com (Admin)")
+  end
 end
