c2163bc809a2ad0fdb7ce70cdd4ff4f6708c2faf
Douglas Costa <dodop3000@gmail.com>
2014-08-10 19:06:28 -0300

    session#create fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.where(:name => params[:signin][:name]).first
-
     if user && user.authenticate(params[:signin][:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Signed in successfully."
+      flash[:notice] = "Signed in successfully."
+      redirect_to root_url
     else
-      flash[:notice] = "Sorry."
+      flash[:error] = "Sorry."
       render :new
     end
   end
